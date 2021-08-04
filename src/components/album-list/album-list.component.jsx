ce8ce77cb890bac1501ab03d7bd0bf2c8365b41b
PatriciaJ <patriciagj@live.com>
2021-08-04 15:32:11 +0100

    created a new component

--- src/components/album-list/album-list.component.jsx
+++ src/components/album-list/album-list.component.jsx
@@ -4,12 +4,17 @@ import { Album } from '../album/album.component';
 
 import './album-list.component.styles.scss';
 
+import albuns from '../../data';
+
 export const AlbumList = props => {
   return (
-    <div className='album-list'>
-      {props.songs.map(song => (
-        <Album key={song.id} name={song.name} />
-      ))}
+    <div className='app'>
+      <h1>Evanescence Discography</h1>
+      <div className='directory-menu'>
+        {albuns.map(({ id, ...otherSectionProps }) => (
+          <Album key={id} {...otherSectionProps} />
+        ))}
+      </div>
     </div>
   );
 };
