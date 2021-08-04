a2727c9200d4fb87e6e70140f277a98191221653
PatriciaJ <patriciagj@live.com>
2021-08-04 11:56:47 +0100

    display the songs accordingly to the album clicked

--- src/page/playlist-page.jsx
+++ src/page/playlist-page.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import {
-  useParams
-} from "react-router-dom";
-import albuns from '../data'
+import { useParams } from 'react-router-dom';
+import albuns from '../data';
 
 import './playlist-page.styles.scss';
 
 export const PlaylistPage = () => {
   const { album } = useParams();
-  console.log(album)
-  const currAlbum = albuns.find((curralbum)=>curralbum.title===album)
-  console.log(currAlbum)
-  return(
-  <div className='playlist'>HERE SHOULD GO THE PLAYLIST</div>
-  )
+  console.log(album);
+  const currAlbum = albuns.find(curralbum => curralbum.title === album);
+  console.log(currAlbum);
+
+  return (
+    <div className='playlist'>
+      Playlist of
+      <h1>{currAlbum.title}</h1>
+      <p>
+        {currAlbum.songs.map((el, id) => (
+          <ol className='song'>
+            {el.id}. {el.name}
+          </ol>
+        ))}
+      </p>
+    </div>
+  );
 };
