00cba23e2cf4cdc4b739c11fa2320b0efa4bffc7
PatriciaJ <patriciagj@live.com>
2021-08-05 17:37:32 +0100

    display the playlist when the button is clicked

--- src/page/playlist-page.jsx
+++ src/page/playlist-page.jsx
@@ -4,23 +4,40 @@ import albuns from '../data';
 
 import './playlist-page.styles.scss';
 
-export const PlaylistPage = () => {
+const AllSongs = () => {
   const { album } = useParams();
   console.log(album);
   const currAlbum = albuns.find(curralbum => curralbum.title === album);
   console.log(currAlbum);
 
   return (
+    <p>
+      {currAlbum.songs.map(el => (
+        <ol className='song'>
+          {el.id}. {el.name}
+        </ol>
+      ))}
+    </p>
+  );
+};
+
+export const PlaylistPage = () => {
+  const { album } = useParams();
+  console.log(album);
+  const currAlbum = albuns.find(curralbum => curralbum.title === album);
+  console.log(currAlbum);
+
+  const [showResults, setShowResults] = React.useState(false);
+  const onClick = () => setShowResults(true);
+
+  return (
     <div className='playlist'>
       Playlist of
       <h1>{currAlbum.title}</h1>
-      <p>
-        {currAlbum.songs.map((el, id) => (
-          <ol className='song'>
-            {el.id}. {el.name}
-          </ol>
-        ))}
-      </p>
+      <button className='btn' onClick={onClick}>
+        SEE ALL SONGS
+      </button>
+      {showResults && <AllSongs />}
     </div>
   );
 };
